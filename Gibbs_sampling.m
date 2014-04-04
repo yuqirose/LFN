6e92255ed5320234757adc3db96227b641f86c22
Rose Yu <yuqi.rose@hotmail.com>
2014-04-03 17:53:56 -0700

    Initial commit

---
+++ Gibbs_sampling.m
@@ -0,0 +1,7 @@
+function [ output_args ] = Gibbs_sampling( input_args )
+%GIBBS_SAMPLING Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ generateLFN.m
@@ -0,0 +1,7 @@
+function [ output_args ] = generateLFN( input_args )
+%GENERATELFN Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
